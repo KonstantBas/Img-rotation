1999e8b896a68db7241ed9adee5cb5ac3a95cb65
KonBas <baskov_konstantin@mail.ru>
2021-12-27 23:16:37 +0300

    Fix bmp

--- bmp.c
+++ bmp.c
@@ -3,74 +3,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define bBC  24;
-#define bT  0x4D42
-
 uint32_t getPadding(const uint64_t w) {
     const size_t widthSize = w * sizeof(struct pixel);
     if (widthSize % 4 == 0) return 0;
     return 4 - (widthSize % 4);
 }
 
-bool from_bmp(FILE* in, struct image* img) {
+enum read_status from_bmp(FILE* in, struct image* img) {
     struct bmp_header header = {0};
     const uint32_t hSize = sizeof(struct bmp_header);
     const uint32_t pSize = sizeof(struct pixel);
     if (fread(&header, hSize, 1, in) < 1) {
-        if (feof(in)) return false;
-        return false;
+        if (feof(in)) return READ_INVALID_BITS;
+        return READ_INVALID_HEADER;
     }
-    if (fseek(in, header.bOffBits, SEEK_SET) != 0) return false;
-    if (header.biBitCount != bBC) return false;
-    if (header.bfType != bT) return false;
+    if (fseek(in, header.bOffBits, SEEK_SET) != 0) return READ_INVALID_BITS;
+    if (header.biBitCount != 24) return READ_ERROR;
+    if (header.bfType != 0x4D42) return READ_ERROR;
 
     img->height = header.biHeight;
     img->width = header.biWidth;
-
-    size_t size = img->width * img->height * pSize
-    img->data = malloc(size);
-
+    img->data = malloc(img->width * img->height * pSize);
     const uint32_t padding = getPadding(img->width);
-    for (size_t j = 0; j < img->height; j++)
-    {
-        if (fread(img->data + j * img->width, pSize, img->width, in) < pSize) return false;
-        if (fseek(in, padding, SEEK_CUR) != 0) return false;
+    for (size_t j = 0; j < img->height; j++) {
+        if (fread(img->data + j * img->width, pSize, img->width, in) < pSize) return READ_INVALID_BITS;
+        if (fseek(in, padding, SEEK_CUR) != 0) return READ_INVALID_BITS;
     }
-    if (fseek(in, 0, SEEK_SET) != 0) return false;
-    return true;
+    if (fseek(in, 0, SEEK_SET) != 0) return READ_INVALID_BITS;
+    return READ_OK;
 }
 
-bool to_bmp(FILE* out, struct image const* img) {
-
-    size_t imgSize = (sizeof(struct pixel) * img->width + img->width % 4) * img->height
-    size_t bfileSize = sizeof(struct bmp_header) + imgSize;
+enum write_status to_bmp(FILE* out, struct image const* img) {
     struct bmp_header header =
             {
-                    .bfType = bT,
-                    .biBitCount = bBC,
+                    .bfType = 0x4D42,
+                    .biBitCount = 24,
                     .biHeight = img->height,
                     .biWidth = img->width,
                     .bOffBits = sizeof(struct bmp_header),
-                    .bfileSize = bfileSize,
+                    .bfileSize = sizeof(struct bmp_header) + (sizeof(struct pixel) * img->width + img->width % 4) * img->height,
                     .biSizeImage = img->width * img->height * sizeof(struct pixel),
                     .biSize = 40,
                     .biPlanes = 1
             };
     size_t tmp = fwrite(&header, sizeof(struct bmp_header), 1, out);
-    if (tmp != 1){ return false; }
+    if (tmp != 1){
+        return WRITE_ERROR;
+    }
     const uint32_t hSize = sizeof(struct bmp_header);
     const uint32_t pSize = sizeof(struct pixel);
-
-    if (fwrite(&header, hSize, 1, out) < 1) return false;
-
+    if (fwrite(&header, hSize, 1, out) < 1) return WRITE_ERROR;
     char paddingBytes[3] = {0};
     const uint32_t padding = getPadding(img->width);
 
     for (size_t j = 0; j < img->height; j++) {
-        if (fwrite(img->data + j * img->width, pSize, img->width, out) != img->width) return false;
-        if (fwrite(paddingBytes, padding, 1, out) != 1 && padding != 0) return false;
-        if (fflush(out) != 0) return false;
+        if (fwrite(img->data + j * img->width, pSize, img->width, out) != img->width) return WRITE_ERROR;
+        if (fwrite(paddingBytes, padding, 1, out) != 1 && padding != 0) return WRITE_ERROR;
+        if (fflush(out) != 0) return WRITE_ERROR;
     }
-    if (fseek(out, 0, SEEK_SET) != 0) return false;
-    return true;
+    if (fseek(out, 0, SEEK_SET) != 0) return WRITE_ERROR;
+    return WRITE_OK;
 }
